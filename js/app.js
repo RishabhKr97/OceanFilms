db37bb182ea0432a5cb581f52030bcc088c7e57d
RishabhKumar <rishabhkr1729@gmail.com>
2016-08-27 19:40:30 +0530

    for hosting

---
+++ js/app.js
@@ -0,0 +1,43 @@
+(function(){
+var app=angular.module('store',[ ]);
+
+
+app.controller('storeController',function(){
+
+this.products=gems;
+
+
+});
+var gems = {
+name: 'ishu',
+price: 14,
+description: 'hello guys my name is ishu and i love coding i love so much',
+soldout: true,     
+canpurchase: true,       
+images: [
+{
+full: 'Burj_Khalifa.jpg',
+thumb: 'Burj_Khalifa.jpg'
+
+},
+{
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+];
+}
+
+}) ();

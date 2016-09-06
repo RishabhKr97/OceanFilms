3b60bffffcea188b948ce92a8a838fa2bbf5d501
ishmeetsingh <ishmeetsingh199@gmail.com>
2016-09-06 18:55:33 +0530

    check

--- theme/assets/pages/scripts/contact-us.js
+++ theme/assets/pages/scripts/contact-us.js
@@ -7,15 +7,15 @@ var ContactUs = function () {
 			$(document).ready(function(){
 			  map = new GMaps({
 				div: '#map',
-	            lat: -13.004333,
-				lng: -38.494333,
+	            lat: 28.7501,
+				lng: 77.1177,
 			  });
 			   var marker = map.addMarker({
-		            lat: -13.004333,
-					lng: -38.494333,
+		            lat: 28.7501,
+					lng: 77.1177,
 		            title: 'Loop, Inc.',
 		            infoWindow: {
-		                content: "<b>Loop, Inc.</b> 795 Park Ave, Suite 120<br>San Francisco, CA 94107"
+		                content: "<b>DTU</b> Shabad Daultpur, Bawana Road<br>New Delhi, India"
 		            }
 		        });
 
